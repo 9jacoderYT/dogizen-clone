29eb7d535f487b0b3d396ae65153c5a94e287a49
Smart Oghenemine <smartjeremy94@gmail.com>
2025-01-11 21:12:53 +0100

    update imports

--- src/app/(site)/page.js
+++ src/app/(site)/page.js
@@ -1,13 +1,13 @@
-import Faq from "@/components/faq";
-import FloatingCredit from "@/components/floating-credit";
-import Footer from "@/components/footer";
-import GameSlider from "@/components/game-slider";
-import HeroSection from "@/components/hero-section";
-import HowToBuy from "@/components/how-to-buy";
-import MediaSection from "@/components/media-section";
-import PartnersSlider from "@/components/partners-sliders";
-import RoadMap from "@/components/roadmap";
-import Tokenomics from "@/components/tokenomics";
+import Faq from "../../components/faq";
+import FloatingCredit from "../../components/floating-credit";
+import Footer from "../../components/footer";
+import GameSlider from "../../components/game-slider";
+import HeroSection from "../../components/hero-section";
+import HowToBuy from "../../components/how-to-buy";
+import MediaSection from "../../components/media-section";
+import PartnersSlider from "../../components/partners-sliders";
+import RoadMap from "../../components/roadmap";
+import Tokenomics from "../../components/tokenomics";
 
 export default function Home() {
   return (
